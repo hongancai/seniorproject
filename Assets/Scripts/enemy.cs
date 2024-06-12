0f299cb2c7d9392537736fe6c280d44777befd6c
Hong An Cai <w44793458@gmail.com>
2024-06-12 17:27:05 +0800

    ADD Scene Img

--- Assets/Scripts/enemy.cs
+++ Assets/Scripts/enemy.cs
@@ -1,19 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    private UnityEngine.AI.NavMeshAgent _agent;
+    public Transform _traget;
+    public NavMeshAgent _agent;
     // Start is called before the first frame update
     void Start()
     {  
-      _agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+      _agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        _agent.SetDestination(GameObject.Find("Tower").transform.position);
+        _agent.SetDestination(_traget.transform.position);
     }
 }
