920ebaff2c29e3b06b8d18b49fb992eb98ba8178
w4479 <w44793458@gmail.com>
2024-08-08 10:42:52 +0800

    Add Data

---
+++ Assets/Scripts/Data/ItemData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemData : MonoBehaviour
+{
+ public int Id {get; set;}
+ public string Name {get; set;}
+ public int SpriteId {get; set;}
+}
--- Assets/Scripts/Shop/ItemHandler.cs
+++ Assets/Scripts/Shop/ItemHandler.cs
@@ -6,8 +6,11 @@ using UnityEngine.UI;
 
 public class ItemHandler : MonoBehaviour
 {
-    public TextMeshProUGUI ItemName;
+    public Text ItemtText;
     public Image ItemImage;
+
+    private bool _isDirty = false;
+    private ItemData _data;
     void Start()
     {
         
@@ -16,11 +19,26 @@ public class ItemHandler : MonoBehaviour
     
     void Update()
     {
-        
+        if (_isDirty)
+        {
+            ItemtText.text = _data.Name;
+            ItemImage.sprite = GameDB.res.itemList[_data.SpriteId];
+            _isDirty = false;
+        }
     }
 
     public void SetImage(Sprite sprite)
     {
         ItemImage.sprite = sprite;
+    }
+    public void SetText(string text)
+    {
+        ItemtText.text = text;
+    }
+
+    public void SetItem(ItemData data)
+    {
+        _data = data;
+        _isDirty = true; //用Update更新
     }
 }
