797fe610850c9f96892c8ee58350258b141e79d0
w4479 <w44793458@gmail.com>
2024-08-20 10:46:15 +0800

    Add GameDB data

--- Assets/Scripts/GameDB.cs
+++ Assets/Scripts/GameDB.cs
@@ -5,10 +5,29 @@ using UnityEngine;
 public static class GameDB 
 {
     //Player
-    public static int playerLives;
-    public static int playerSpeed;
+    public static float playerHp;
+    public static float playerSpd;
+    public static float playerAtk;
+    public static float playerDef;
+    public static float playerAtkSpd;
+    public static float playerexp;
     
     //Shop
     public static Res res;
-
+    
+    //Enemy
+    public static float enemyHp;
+    public static float enemySpd;
+    public static float enemyAtk;
+    public static float enemyDef;
+    public static float enemyAtkSpd;
+    
+    //Tower
+    public static float towerexp;
+    public static float towerHp;
+    public static float towerSpd;
+    public static float towerAtk;
+    public static float towerDef;
+    public static float towerAtkSpd;
+    
 }
--- Assets/Scripts/Shop/TestShopMgr.cs
+++ Assets/Scripts/Shop/TestShopMgr.cs
@@ -1,43 +1,55 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class TestShopMgr : MonoBehaviour
 {
     public GameObject ItemPrefab;
     
     public GameObject ContentPool;
+
+    private IList<ItemData> list;
+
+    void Awake()
+    {
+        string json = PlayerPrefs.GetString("data");
+    }
+    
+    public IList<ItemData> GetData()
+    {
+        //fromDB
+        list = new List<ItemData>();
+        IList<ItemData> result = new List<ItemData>();
+        result.Add(new ItemData() {Id = 0, Name = "麵線" , SpriteId = 0 });
+        result.Add(new ItemData() {Id = 1, Name = "高粱酒" , SpriteId = 1 });
+        result.Add(new ItemData() {Id = 2, Name = "貢糖" , SpriteId = 2 });
+        result.Add(new ItemData() {Id = 3, Name = "牛肉乾" , SpriteId = 3 });
+        
+        return result;
+    }
+
     void Start()
     {
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < list.Count; i++)
         {
             GameObject temp = Instantiate(ItemPrefab);
             temp.transform.parent = ContentPool.transform;
-            //temp.transform.localPosition = Vector3.zero;
-            temp.transform.localEulerAngles = Vector3.zero;
             temp.transform.localScale = Vector3.one;
-            ItemHandler handler = temp.GetComponent<ItemHandler>();
-            int _rndIndex = Random.Range(0,GameDB.res.itemList.Count);
-            handler.SetImage(GameDB.res.itemList[_rndIndex]);
 
-            int temp_I = i;
-            temp.GetComponent<Button>().onClick.AddListener(
-                () =>
-                {
-                    OnItemClick(temp_I);
-                }
-                );
+            ItemHandler handler = temp.GetComponent<ItemHandler>();
+            //int _rndIndex = Random.Range(0,GameDB.res.itemList.Count);
+            // handler.SetImage(GameDB.res.itemList[_rndIndex]);
+            handler.SetItem(list[i]);
         }
     }
-    
+
     void Update()
     {
         
     }
-
-    public void OnItemClick(int itemId)
-    {
-        Debug.Log(itemId);
+    
+   
     }
-}
