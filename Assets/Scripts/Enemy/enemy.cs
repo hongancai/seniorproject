06863ae060ab782547a206c6e7cab0db123edf76
w4479 <w44793458@gmail.com>
2024-10-21 16:19:12 +0800

    Add PlayerDamage Script

--- Assets/Scripts/Enemy/enemy.cs
+++ Assets/Scripts/Enemy/enemy.cs
@@ -8,14 +8,14 @@ public class Enemy : MonoBehaviour
     public Transform _traget;
     public NavMeshAgent _agent;
     //public int hitPoints = 3;
-    // Start is called before the first frame update
+    
     void Start()
     {  
       _agent = GetComponent<NavMeshAgent>();
       GameDB.enemyHp = 3;
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         _agent.SetDestination(_traget.transform.position);
--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
@@ -12,16 +13,34 @@ public class PlayerHealth : MonoBehaviour
     {
         playerDiePnl.SetActive(false);
         GameDB.playerHp = 10;
+        UpdateHealthSlider();
+        rebirthBtn.onClick.AddListener(OnRebirth);
     }
-
-   
+    
     void Update()
     {
         if (GameDB.playerHp <= 0)
         {
-            playerDiePnl.SetActive(true);
+            Die();
         }
     }
+
+    public void UpdateHealthSlider()
+    {
+        healthSlider.value = GameDB.playerHp;
+    }
+
+    public void Die()
+    {
+        playerDiePnl.SetActive(true);
+    }
     
+    private void OnRebirth()
+    {
+        GameDB.playerHp = 10;           // 重置玩家血量
+        playerDiePnl.SetActive(false);   // 隱藏死亡面板
+        UpdateHealthSlider(); 
+        SceneManager.LoadScene("上");
+    }
     
 }
