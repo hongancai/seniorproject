9739a06bef032afee2a0d6a47797a0867ebae5d6
w4479 <w44793458@gmail.com>
2025-03-21 11:36:53 +0800

    Add S1 陰影 && Buff進度1

---
+++ Assets/Scripts/ItemEffect/Buff.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buff 
+{
+    public int HP;
+    public int Atk;
+    public int Def;
+    public float SpeedRate;
+}
