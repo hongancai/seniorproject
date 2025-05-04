2ebe718ccf9180767cddb30de578208e9d41351f
w4479 <w44793458@gmail.com>
2025-05-04 13:40:44 +0800

    修改Sequence monster prefabs

---
+++ Assets/Scripts/Enemy/LFishmanMgr.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class LFishmanMgr : MonoBehaviour
+{
+    public Transform traget;
+    public NavMeshAgent agent;
+    
+    private Animator animator;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+  
+    void Update()
+    {
+        agent.SetDestination(traget.transform.position);
+        animator.Play("L_move");
+        gameObject.transform.eulerAngles = new Vector3(30, 0, 0);
+        var vector3 = gameObject.transform.position;
+        vector3.y = 0.622f; //強制更新高度
+        gameObject.transform.position = vector3;
+    }
+}
--- Assets/Scripts/Enemy/RGhostMgr.cs
+++ Assets/Scripts/Enemy/RGhostMgr.cs
@@ -3,24 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class SoliderEnemy : MonoBehaviour
+public class RGhostMgr : MonoBehaviour
 {
     public Transform traget;
     public NavMeshAgent agent;
     
     private Animator animator;
-    
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
     }
 
-    
+  
     void Update()
     {
-        gameObject.transform.eulerAngles = new Vector3(30, 0, 0);
         agent.SetDestination(traget.transform.position);
-        animator.Play("R_move"); 
+        animator.Play("R_move");
+        gameObject.transform.eulerAngles = new Vector3(30, 0, 0);
+        var vector3 = gameObject.transform.position;
+        vector3.y = 0.81f; //強制更新高度
+        gameObject.transform.position = vector3;
     }
 }
